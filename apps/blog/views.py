f2c9133816928ffd3015e8c52689d9bd04b671dc
Asror Kabulov <ansoriy559@gmail.com>
2023-06-07 18:06:44 +0600

    add templates

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-from django.http import HttpResponse
 
-def HomePageView(request):
-    return HttpResponse('Hello World')
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
