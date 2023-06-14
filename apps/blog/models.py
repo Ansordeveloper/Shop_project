8cbbfc0f77ed8afab6835df1a82f73944db3fbac
Asror Kabulov <ansoriy559@gmail.com>
2023-06-14 09:54:28 +0600

    add registration

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Post(models.Model):
+    title = models.CharField(max_length=100) # Mavzu
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE) # foydalanuvchi 
+    text = models.TextField() # Matn
 
+
+    def __str__(self):
+        return self.title
+    
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-
-
-
-class HomePageView(TemplateView):
-    template_name = 'home.html'
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView,DeleteView
+from django.urls import reverse_lazy
+from .models import Post
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'new.html'
+    fields = ['title', 'author', 'text']
+    success_url = reverse_lazy('home')
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'text']
+    success_url = reverse_lazy('home')
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home') 
